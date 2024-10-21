7add3bd81d95505275eef45ffcfa32bea7c4d096
emsi76 <michael@cocheteau.de>
2024-10-21 09:45:31 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -55,12 +55,12 @@ service_exists() {
 get_rclone_webdav(){
 	echo 'get the rclone_webdav files into $RCLONE_WEBDAV_FOLDER from $repoUrl'
 	mkdir -p $RCLONE_WEBDAV_FOLDER
- 	wget -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.sh" "$repoUrl/rclone_webdav/rclone_webdav.sh"
-	wget -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.service" "$repoUrl/rclone_webdav/rclone_webdav.service"
- 	echo 'getting config - but no overrwrite'
-	wget -nc -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.env" "$repoUrl/rclone_webdav/rclone_webdav.env"
- 	echo 'getting htpasswd - but no overrwrite'
-	wget -nc -q -O "$RCLONE_WEBDAV_FOLDER/htpasswd" "$repoUrl/rclone_webdav/htpasswd"
+ 	wget -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.sh" "$repoUrl/rclone_webdav/rclone_webdav.sh"
+	wget -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.service" "$repoUrl/rclone_webdav/rclone_webdav.service"
+ 	echo 'get config - but no overrwrite'
+	wget -nc -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.env" "$repoUrl/rclone_webdav/rclone_webdav.env"
+ 	echo 'get htpasswd - but no overrwrite'
+	wget -nc -O "$RCLONE_WEBDAV_FOLDER/htpasswd" "$repoUrl/rclone_webdav/htpasswd"
  	echo 'setting right permissions'
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.sh
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.service

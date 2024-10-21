d2b02d0e34e1f6a65fc109eb9e929cdb93dd8542
emsi76 <michael@cocheteau.de>
2024-10-21 09:33:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -57,13 +57,13 @@ get_rclone_webdav(){
 	mkdir -p $RCLONE_WEBDAV_FOLDER
  	wget -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.sh" "$repoUrl/rclone_webdav/rclone_webdav.sh"
 	wget -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.service" "$repoUrl/rclone_webdav/rclone_webdav.service"
- 	echo getting config and htpasswd if not existing
+ 	echo getting config and htpasswd (but no overrwrite)
 	wget -nc -q -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.env" "$repoUrl/rclone_webdav/rclone_webdav.env"
 	wget -nc -q -O "$RCLONE_WEBDAV_FOLDER/htpasswd" "$repoUrl/rclone_webdav/htpasswd"
  	echo setting right permissions
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.sh
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.service
- 	echo rclone_webdav now in $RCLONE_WEBDAV_FOLDER
+ 	echo rclone_webdav now into $RCLONE_WEBDAV_FOLDER
 }
 
 install_service(){

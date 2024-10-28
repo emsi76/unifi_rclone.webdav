c3dff165cd7ac4697a7c857d539c5d6ad6a08f58
MC76 <michael@cocheteau.de>
2024-10-28 21:16:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -58,16 +58,18 @@ get_rclone_webdav(){
  	wget -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.sh" "$repoUrl/rclone_webdav/rclone_webdav.sh"
 	wget -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.service" "$repoUrl/rclone_webdav/rclone_webdav.service"
  	wget -O "$RCLONE_WEBDAV_FOLDER/ban_failed_users.sh" "$repoUrl/rclone_webdav/ban_failed_users.sh"
+ 	wget -O "$RCLONE_WEBDAV_FOLDER/ban_failed_users.service" "$repoUrl/rclone_webdav/ban_failed_users.service"
 	(wget -nc -O "$RCLONE_WEBDAV_FOLDER/rclone_webdav.env" "$repoUrl/rclone_webdav/rclone_webdav.env" || true)
 	(wget -nc -O "$RCLONE_WEBDAV_FOLDER/htpasswd" "$repoUrl/rclone_webdav/htpasswd" || true)
  	echo 'setting right permissions'
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.sh
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/ban_failed_users.sh
 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/rclone_webdav.service
+ 	chmod oug+rx $RCLONE_WEBDAV_FOLDER/ban_failed_users.service
  	echo 'rclone_webdav now into $RCLONE_WEBDAV_FOLDER'
 }
 
-install_service(){
+install_webdav_service(){
 	echo install service 'rclone_webdav.service'
 	cp $RCLONE_WEBDAV_FOLDER/rclone_webdav.service  /etc/systemd/system/rclone_webdav.service
 	sudo systemctl daemon-reload
@@ -76,7 +78,7 @@ install_service(){
  	echo service 'rclone_webdav.service' installed
 }
 
-update_service(){
+update_webdav_service(){
 	echo update service 'rclone_webdav.service'
 	sudo systemctl stop rclone_webdav.service
 	sudo systemctl disable rclone_webdav.service
@@ -87,11 +89,39 @@ update_service(){
  	echo service 'rclone_webdav.service' updated
 }
 
-uninstall_service(){
+uninstall_webdav_service(){
 	echo uninstall service 'rclone_webdav.service'
 	sudo systemctl stop rclone_webdav.service
 	sudo systemctl disable rclone_webdav.service
 	rm $RCLONE_WEBDAV_FOLDER/rclone_webdav.service
+	sudo systemctl daemon-reload
+}
+
+install_ban_failed_users_service(){
+	echo install service 'ban_failed_users.service'
+	cp $RCLONE_WEBDAV_FOLDER/ban_failed_users.service  /etc/systemd/system/ban_failed_users.service
+	sudo systemctl daemon-reload
+	sudo systemctl start ban_failed_users.service
+	sudo systemctl enable ban_failed_users.service
+ 	echo service 'ban_failed_users.service' installed
+}
+
+update_ban_failed_users_service(){
+	echo update service 'ban_failed_users.service'
+	sudo systemctl stop ban_failed_users.service
+	sudo systemctl disable ban_failed_users.service
+	cp $RCLONE_WEBDAV_FOLDER/ban_failed_users.service  /etc/systemd/system/ban_failed_users.service
+	sudo systemctl daemon-reload
+	sudo systemctl start ban_failed_users.service
+	sudo systemctl enable ban_failed_users.service
+ 	echo service 'ban_failed_users.service' updated
+}
+
+uninstall_ban_failed_users_service(){
+	echo uninstall service 'ban_failed_users.service'
+	sudo systemctl stop ban_failed_users.service
+	sudo systemctl disable ban_failed_users.service
+	rm $RCLONE_WEBDAV_FOLDER/ban_failed_users.service
 	sudo systemctl daemon-reload
 }
 
@@ -109,10 +139,18 @@ install_rclone
 
 if service_exists rclone_webdav; 
 	then
-		update_service
+		update_webdav_service
 		echo service updated
     	else
-		install_service
+		install_webdav_service
+		echo service installed
+fi
+if service_exists ban_failed_users; 
+	then
+		update_ban_failed_users_service
+		echo service updated
+    	else
+		install_ban_failed_users_service
 		echo service installed
 fi
 

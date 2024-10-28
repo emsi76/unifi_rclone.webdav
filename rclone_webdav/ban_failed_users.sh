b1cc0228a33f88dd3e3055ca5e22d11fb643ce94
MC76 <michael@cocheteau.de>
2024-10-28 20:55:07 +0100

    Update ban_failed_users.sh

--- rclone_webdav/ban_failed_users.sh
+++ rclone_webdav/ban_failed_users.sh
@@ -7,7 +7,7 @@ set +a
 
 tail -f --pid $(pidof rclone) ${RCLONE_WEBDAV_LOG_PATH} | grep --line-buffered "^$(date +'%Y/%m/%d').*Unauthorized request.*from\s[$(grep -o '^[aA-zZ]*[^:]' /data/rclone/htpasswd | paste -s -d '|')]" | grep --line-buffered -o '\S*$' | while read line;
     do
-        if grep -c "^$(date +'%Y/%m/%d').*Unauthorized request.*from\swebdav" ${RCLONE_WEBDAV_LOG_PATH} > $(RCLONE_WEBDAV_FAILED_LIMIT)
+        if grep -c "^$(date +'%Y/%m/%d').*Unauthorized request.*from\swebdav" ${RCLONE_WEBDAV_LOG_PATH} > ${RCLONE_WEBDAV_FAILED_LIMIT}
         then sed -i "s/^$line/#$line/g" /data/rclone/htpasswd
     fi
 done
